455d51c619dcd12e2b60dbaf84e82f7d80129a7c
Jesse-Justin Mdachi <jessejustinm@gmail.com>
2023-05-08 17:12:31 +0300

    randomly generate dice values

--- lib/widgets/gradient_container.dart
+++ lib/widgets/gradient_container.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice/widgets/styled_text.dart';
+import 'package:roll_dice/widgets/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -16,25 +16,7 @@ class GradientContainer extends StatelessWidget {
         gradient: LinearGradient(
             colors: colors, begin: startAlignment, end: endAlignment),
       ),
-      child: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            'assets/images/dice-3.png',
-            width: 200,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(fontSize: 20)),
-              child: const Text('Roll dice'))
-        ],
-      )),
+      child: const DiceRoller(),
     );
   }
 
